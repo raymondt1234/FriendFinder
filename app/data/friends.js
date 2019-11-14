9fbd2f819af776785ff9ed4f5a666a1dc66f732b
Raymond _ <raymondt1234@gmail.com>
2019-11-14 10:38:51 -0700

    Started home.html and survey.html

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+let friends = [
+    {
+        "name": "",
+        "photo": "",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+]
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,13 @@
+let express = require("express");
+let app = express();
+let PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`);
+});
