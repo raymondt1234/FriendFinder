7dc70f3ac953b55d7dc8b9a8c09c13b46fac9ae5
Raymond _ <raymondt1234@gmail.com>
2019-11-14 11:31:40 -0700

    Servey questions added

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,50 @@
 let friends = [
     {
-        "name": "",
-        "photo": "",
+        "name": "Cat",
+        "photo": "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages5.fanpop.com%2Fimage%2Fphotos%2F32000000%2FCat-cats-32040775-1440-900.jpg&f=1&nofb=1",
         "scores": [
             5,
             1,
-            4,
-            4,
-            5,
             1,
+            1,
+            1,
+            1,
+            5,
+            5,
+            2,
+            1
+        ]
+    },
+    {
+        "name": "dog",
+        "photo": "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.wikihow.com%2Fimages%2F6%2F64%2FStop-a-Dog-from-Jumping-Step-6-Version-2.jpg&f=1&nofb=1",
+        "scores": [
+            2,
+            3,
+            5,
+            3,
+            5,
+            3,
+            2,
             2,
             5,
-            4,
             1
+        ]
+    },
+    {
+        "name": "capybara",
+        "photo": "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fstatic.boredpanda.com%2Fblog%2Fwp-content%2Fuploads%2F2016%2F04%2Fcapybara-unusual-animal-friendship-fb.png&f=1&nofb=1",
+        "scores": [
+            5,
+            5,
+            2,
+            5,
+            1,
+            5,
+            1,
+            1,
+            1,
+            5
         ]
     }
 ]
