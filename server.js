987c4c12c7e0c6e9b7ec188cf1233e6801dcd8f1
raymondt1234 <raymondt1234@gmail.com>
2019-11-26 18:29:49 -0700

    fixed a few errors in the code

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log(`App listening on PORT: ${PORT}`);
