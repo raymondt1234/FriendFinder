8a229845ffc1f9a2de0270da761fe047381d350f
Raymond _ <raymondt1234@gmail.com>
2021-05-12 21:18:48 -0700

    Best match returned when the survey is submitted

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,29 @@
 const friends = require("../data/friends");
 
 module.exports = function (app) {
-    app.get("/api/friends", function(req, res){
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
+    });
+    app.post("/api/friends", function (req, res) {
+        
+        let userScores = req.body.scores;
+        
+        let bestMatch;
+
+        let lowestDifference = 50;
+        // Set to 50 so that the first friend will start as bestMatch until another friend scores lower.
+
+        friends.forEach(friend => {
+            let totalDifference = 0;
+            for (let i = 0; i < friend.scores.length; i++) {
+                totalDifference += Math.abs(friend.scores[i] - userScores[i]);
+            }
+
+            if (totalDifference < lowestDifference) {
+                lowestDifference = totalDifference;
+                bestMatch = friend;
+            }
+        });
+        res.json(JSON.stringify(bestMatch));
     });
 };
